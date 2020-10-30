fd57dced0f3843c4288b63643d0ca16536e73747
Simen Abelsen <simennab@protonmail.com>
2020-10-30 23:10:01 +0100

    Refactor to Title parser, search for multiple tags

--- lib/foresight/parser.ex
+++ lib/foresight/parser.ex
@@ -2,13 +2,10 @@ defmodule Foresight.Parser do
   @moduledoc """
   A parser to get meta information
   """
+  alias Foresight.Parser.{Title}
 
   def get_title(doc) do
-    doc
-    |> Floki.parse_document!
-    |> Floki.find("title")
-    |> List.first()
-    |> Floki.text()
+    Title.search_title(doc)
   end
 
   def get_meta_tags(doc) do
---
+++ lib/foresight/parser/title.ex
@@ -0,0 +1,32 @@
+defmodule Foresight.Parser.Title do
+  @moduledoc """
+  A parser to get the title of a given html document
+  """
+  @title_tags ["meta[property='og:title']", "title"]
+
+  def search_title(doc) do
+    doc
+    |> Floki.parse_document!
+    |> find_title(@title_tags)
+    |> extract_title()
+  end
+
+  defp find_title(doc, [tag | tail]) do
+    case Floki.find(doc, tag) do
+      [] -> find_title(doc, tail)
+      [ title ] -> title
+    end
+  end
+
+  defp find_title(_doc, []) do
+    nil
+  end
+
+  defp extract_title(nil) do
+    nil
+  end
+
+  defp extract_title(node) do
+    Floki.text(node)
+  end
+end
