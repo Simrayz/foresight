b2798dfa82230a7cb01f66517b5ed309aaabd3bc
Simen Abelsen <simennab@protonmail.com>
2020-10-31 01:59:48 +0100

    Add documentation and moduledocs to context modules.

--- lib/foresight/parser.ex
+++ lib/foresight/parser.ex
@@ -4,18 +4,22 @@ defmodule Foresight.Parser do
   """
   alias Foresight.Parser.{Title, Description, Image}
 
+  @doc "A context function to abstract the underlying parser implementation of titles"
   def get_title(doc) do
     Title.search_title(doc)
   end
 
+  @doc "A context function to abstract the underlying parser implementation of descriptions"
   def get_description(doc) do
     Description.search_description(doc)
   end
 
+  @doc "A context function to abstract the underlying parser implementation of image url fetching"
   def get_image(doc) do
     Image.search_image(doc)
   end
 
+  @doc "A context function to get all <meta> tags from the raw html body"
   def get_meta_tags(doc) do
     doc
     |> Floki.parse_document!
