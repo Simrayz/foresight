91c2da04862d39f484400d3bcc89d287a7ecf63a
Simen Abelsen <simennab@protonmail.com>
2020-11-01 20:08:01 +0100

    Add description fields for documentation generation

--- mix.exs
+++ mix.exs
@@ -7,7 +7,15 @@ defmodule Foresight.MixProject do
       version: "0.1.0",
       elixir: "~> 1.11",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+
+      # Doc fields
+      name: "Foresight",
+      source_urL: "https://github.com/Simrayz/foresight",
+      docs: [
+        main: "Foresight"
+      ]
+
     ]
   end
 
@@ -22,7 +30,8 @@ defmodule Foresight.MixProject do
   defp deps do
     [
       {:httpoison, "~> 1.6"},
-      {:floki, "~> 0.29.0"}
+      {:floki, "~> 0.29.0"},
+      {:ex_doc, "~> 0.23", only: :dev, runtime: false}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
     ]
