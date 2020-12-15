182022dbb803785e5c14887aa1f2eb9c65bc8c5e
Simen Abelsen <simennab@protonmail.com>
2020-12-15 02:17:48 +0100

    Add phoenix to project, and add simple API endpoints

--- config/config.exs
+++ config/config.exs
@@ -1,2 +1,31 @@
+# This file is responsible for configuring your application
+# and its dependencies with the aid of the Mix.Config module.
+#
+# This configuration file is loaded before any dependency and
+# is restricted to this project.
+
+# General application configuration
 use Mix.Config
+
+# Configures the endpoint
+config :foresight, ForesightWeb.Endpoint,
+  url: [host: "localhost"],
+  secret_key_base: "9zF7z7H6F2XIYrtn27HEVZAFskC7X2M1wGlFRYJWVlYTBXWkslOZBK5gC7Annhoe",
+  render_errors: [view: ForesightWeb.ErrorView, accepts: ~w(json), layout: false],
+  pubsub_server: Foresight.PubSub,
+  live_view: [signing_salt: "5YH/pLzY"]
+
+# Configures Elixir's Logger
+config :logger, :console,
+  format: "$time $metadata[$level] $message\n",
+  metadata: [:request_id]
+
+# Use Jason for JSON parsing in Phoenix
+config :phoenix, :json_library, Jason
+
+# Use Html5ever for HTML parsing in Floki
 config :floki, :html_parser, Floki.HTMLParser.Html5ever
+
+# Import environment specific config. This must remain at the bottom
+# of this file so it overrides the configuration defined above.
+import_config "#{Mix.env()}.exs"
--- mix.exs
+++ mix.exs
@@ -6,25 +6,31 @@ defmodule Foresight.MixProject do
       app: :foresight,
       version: "0.1.1",
       elixir: "~> 1.11",
+      elixirc_paths: elixirc_paths(Mix.env()),
+      compilers: [:phoenix, :gettext] ++ Mix.compilers(),
       start_permanent: Mix.env() == :prod,
       deps: deps(),
-
+      aliases: aliases(),
       # Doc fields
       name: "Foresight",
       source_urL: "https://github.com/Simrayz/foresight",
       docs: [
         main: "Foresight"
       ]
-
     ]
   end
 
   # Run "mix help compile.app" to learn about applications.
   def application do
     [
-      extra_applications: [:logger]
+      mod: {Foresight.Application, []},
+      extra_applications: [:logger, :runtime_tools]
     ]
   end
+
+  # Specifies which paths to compile per environment.
+  defp elixirc_paths(:test), do: ["lib", "test/support"]
+  defp elixirc_paths(_), do: ["lib"]
 
   # Run "mix help deps" to learn about dependencies.
   defp deps do
@@ -32,9 +38,23 @@ defmodule Foresight.MixProject do
       {:httpoison, "~> 1.6"},
       {:floki, "~> 0.29.0"},
       {:html5ever, "~> 0.8.0"},
-      {:ex_doc, "~> 0.23", only: :dev, runtime: false}
+      {:ex_doc, "~> 0.23", only: :dev, runtime: false},
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      # Phoenix Deps
+      {:phoenix, "~> 1.5.7"},
+      {:phoenix_live_dashboard, "~> 0.4"},
+      {:telemetry_metrics, "~> 0.4"},
+      {:telemetry_poller, "~> 0.4"},
+      {:gettext, "~> 0.11"},
+      {:jason, "~> 1.0"},
+      {:plug_cowboy, "~> 2.0"}
+    ]
+  end
+
+  defp aliases do
+    [
+      setup: ["deps.get"]
     ]
   end
 end
