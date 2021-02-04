a7e2b08871122439717f33b67634b242fdcb5e9f
Simen Abelsen <simenabelsen@hotmail.no>
2021-02-04 16:50:01 +0100

    v0.2.2

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Foresight.MixProject do
   def project do
     [
       app: :foresight,
-      version: "0.2.1",
+      version: "0.2.2",
       elixir: "~> 1.11",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
